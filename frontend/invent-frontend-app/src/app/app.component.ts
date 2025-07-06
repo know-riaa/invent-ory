d4fd492008f2867d2cbd1a76f62dad2fd3b3fe3f
know-riaa <knowriaa@gmail.com>
2025-07-06 19:09:29 -0400

    Adding everything

--- frontend/invent-frontend-app/src/app/app.component.ts
+++ frontend/invent-frontend-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SearchBarComponent],
+  imports: [RouterOutlet, SearchBarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,7 +21,7 @@ export class AppComponent {
   userData(query:string){
     console.log('User Input is :', query);
 
-    this.http.get<any[]>(`https://eobsobsos${query}`)
+    this.http.get<any[]>(`http://localhost:8080/main/search/${query}`)
     .subscribe({
       next: (res) =>  {
         this.response = res;

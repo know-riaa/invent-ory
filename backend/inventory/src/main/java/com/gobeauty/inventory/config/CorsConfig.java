d12b3d5cddfcb61c01888d0ae3bc4b3b345c3677
know-riaa <knowriaa@gmail.com>
2025-07-06 19:22:22 -0400

    Adding cors

---
+++ backend/inventory/src/main/java/com/gobeauty/inventory/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.gobeauty.inventory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200", "http://inventory-frontend-app.s3-website-us-east-1.amazonaws.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
